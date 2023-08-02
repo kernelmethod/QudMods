de6274f1fde1124a0f4ab420f9956df7322a34a0
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2023-08-01 18:52:00 -0700

    Simplify Options.cs

--- CrungleMode/CrungleMode/Scripts/Options.cs
+++ CrungleMode/CrungleMode/Scripts/Options.cs
@@ -4,56 +4,16 @@ namespace Kernelmethod.CrungleMode {
     /// when starting a new game.
     /// </summary>
     public class Options {
-        public static bool EnableBasicSurvivalOptions {
-            get {
-                return GetOption("Option_Kernelmethod_CrungleMode_BasicSurvival") == "Yes";
-            }
-        }
+        public static bool EnableBasicSurvivalOptions => GetOption("Option_Kernelmethod_CrungleMode_BasicSurvival").EqualsNoCase("Yes");
+        public static bool SpawnWithMakeCamp => EnableBasicSurvivalOptions || GetOption("Option_Kernelmethod_CrungleMode_SpawnWithMakeCamp").EqualsNoCase("Yes");
+        public static bool SpawnWithSprint => EnableBasicSurvivalOptions || GetOption("Option_Kernelmethod_CrungleMode_SpawnWithSprint").EqualsNoCase("Yes");
+        public static bool SpawnWithWater => EnableBasicSurvivalOptions || GetOption("Option_Kernelmethod_CrungleMode_SpawnWithWater").EqualsNoCase("Yes");
+        public static bool SpawnWithLightSources => EnableBasicSurvivalOptions || GetOption("Option_Kernelmethod_CrungleMode_SpawnWithTorches").EqualsNoCase("Yes");
 
-        public static bool SpawnWithMakeCamp {
-            get {
-                return EnableBasicSurvivalOptions || GetOption("Option_Kernelmethod_CrungleMode_SpawnWithMakeCamp") == "Yes";
-            }
-        }
+        public static bool NoAquatic => GetOption("Option_Kernelmethod_CrungleMode_NoAquatic").EqualsNoCase("Yes");
+        public static bool NoLivesOnWalls => GetOption("Option_Kernelmethod_CrungleMode_NoLivesOnWalls").EqualsNoCase("Yes");
+        public static bool SaveScores => GetOption("Option_Kernelmethod_CrungleMode_SaveScores").EqualsNoCase("Yes");
 
-        public static bool SpawnWithSprint {
-            get {
-                return EnableBasicSurvivalOptions || GetOption("Option_Kernelmethod_CrungleMode_SpawnWithSprint") == "Yes";
-            }
-        }
-
-        public static bool SpawnWithWater {
-            get {
-                return EnableBasicSurvivalOptions || GetOption("Option_Kernelmethod_CrungleMode_SpawnWithWater") == "Yes";
-            }
-        }
-
-        public static bool SpawnWithLightSources {
-            get {
-                return EnableBasicSurvivalOptions || GetOption("Option_Kernelmethod_CrungleMode_SpawnWithTorches") == "Yes";
-            }
-        }
-
-        public static bool NoAquatic {
-            get {
-                return GetOption("Option_Kernelmethod_CrungleMode_NoAquatic") == "Yes";
-            }
-        }
-
-        public static bool NoLivesOnWalls {
-            get {
-                return GetOption("Option_Kernelmethod_CrungleMode_NoLivesOnWalls") == "Yes";
-            }
-        }
-
-        public static bool SaveScores {
-            get {
-                return GetOption("Option_Kernelmethod_CrungleMode_SaveScores") == "Yes";
-            }
-        }
-
-        private static string GetOption(string ID, string Default = "") {
-            return XRL.UI.Options.GetOption(ID, Default=Default);
-        }
+        private static string GetOption(string ID, string Default = "") => XRL.UI.Options.GetOption(ID, Default: Default);
     }
 }
