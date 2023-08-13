4fcfc79ba8526fe10b696338c7ceba81d83df2d8
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2023-08-13 15:11:57 -0700

    Fix issue where only one secret could be learned.
    Fixed a bug where only one secret learned during Precognition could be
    retained. There's currently a lingering problem where recipes are not
    retained, but otherwise the mod is working as expected.

--- PrecogSecrets/PrecogSecrets/Parts/PrecogSecretHandler.cs
+++ PrecogSecrets/PrecogSecrets/Parts/PrecogSecretHandler.cs
@@ -11,7 +11,7 @@ namespace Kernelmethod.PrecogSecrets {
     public class PrecogSecretHandler : IPart {
         public static string Prefix = "Kernelmethod_PrecogSecrets";
 
-        public Dictionary<string, bool> RevealedSecrets = new Dictionary<string, bool>();
+        public Dictionary<string, bool> KnownSecrets = new Dictionary<string, bool>();
         public bool Activated = false;
 
         public override bool WantEvent(int ID, int cascade) {
@@ -33,13 +33,13 @@ namespace Kernelmethod.PrecogSecrets {
         }
 
         public override bool HandleEvent(GetPrecognitionRestoreGameStateEvent E) {
-            MetricsManager.LogInfo($"{Prefix}: Saving secret state to state dictionary");
+            LogInfo("saving secret state to game dictionary");
 
             // Register the secrets that have been learned or forgotten to the state dictionary
             // prior to reverting to the previous save.
-            foreach (KeyValuePair<string, bool> secretState in RevealedSecrets) {
+            foreach (KeyValuePair<string, bool> secretState in KnownSecrets) {
                 var gameStateKey = GetSecretTransferKey(secretState.Key);
-                MetricsManager.LogInfo($"{Prefix}: saving state known = {secretState.Value} for secret {secretState.Key} in {gameStateKey}");
+                LogInfo($"saving state known = {secretState.Value} for secret {secretState.Key} in {gameStateKey}");
                 E.Set(gameStateKey, secretState.Value);
             }
 
@@ -50,20 +50,63 @@ namespace Kernelmethod.PrecogSecrets {
             if (E.Notify != "PrecognitionGameRestored")
                 return base.HandleEvent(E);
 
-            MetricsManager.LogInfo($"{Prefix}: Restoring learned and forgotten secrets");
+            try {
+                RecallSecrets();
+            }
+            catch (Exception ex) {
+                LogException("error recalling secret state:", ex);
+            }
+            return base.HandleEvent(E);
+        }
 
-            // Restore learned secrets from the game's state dictionary
+        public void RecallSecrets() {
+            LogInfo("finding learned and forgotten secrets");
+
+            // Construct a dictionary with secrets to remember/forget
+            var secrets = new Dictionary<string, bool>();
             foreach (KeyValuePair<string, bool> gameState in The.Game.BooleanGameState) {
-                MetricsManager.LogInfo($"{Prefix}: Checking gameState.Key = {gameState.Key}");
+                if (!gameState.Key.StartsWith(Prefix))
+                    continue;
 
-                if (gameState.Key.StartsWith(Prefix)) {
-                    var secretid = gameState.Key.Substring(Prefix.Length + 1);
-                    RestoreSecretState(secretid, gameState.Value);
-                    The.Game.RemoveBooleanGameState(gameState.Key);
-                }
+                var secretid = gameState.Key.Substring(Prefix.Length + 1);
+                secrets[secretid] = gameState.Value;
+                LogInfo($"extracted secret {secretid} from game state dictionary");
             }
 
-            return base.HandleEvent(E);
+            // Remove keys from the game's state dictionary
+            foreach (KeyValuePair<string, bool> secret in secrets) {
+                var key = GetSecretTransferKey(secret.Key);
+                The.Game.RemoveBooleanGameState(key);
+                LogInfo($"removed {key} from the game state dictionary");
+            }
+
+            LogInfo("restoring learned and forgotten secrets");
+
+            // Search for journal entries with the IDs that were inserted into the dictionary, and
+            // use them to remember or forget secrets.
+            foreach (IBaseJournalEntry entry in JournalAPI.GetAllNotes()) {
+                var secretid = entry.secretid;
+                if (secretid == null || !secrets.ContainsKey(secretid))
+                    continue;
+
+                var known = secrets[entry.secretid];
+                LogInfo($"restoring secret {secretid} (known = {known})");
+
+                if (known)
+                    entry.Reveal(silent: true);
+                else
+                    entry.Forget();
+
+                // Pop key from the dictionary
+                secrets.Remove(entry.secretid);
+
+                // Exit if there are no more secrets left to process
+                if (secrets.Count == 0)
+                    break;
+            }
+
+            // Deactivate the part
+            Activated = false;
         }
 
         public override bool FireEvent(Event E) {
@@ -72,36 +115,33 @@ namespace Kernelmethod.PrecogSecrets {
             else if (E.ID == "BeforeSecretRevealed" && Activated) {
                 // Register that the secret was revealed
                 IBaseJournalEntry secret = E.GetParameter<IBaseJournalEntry>("Secret");
-                RevealedSecrets[secret.secretid] = true;
-                MetricsManager.LogInfo($"{Prefix}: learned secret {secret.secretid} during precognition");
+                if (secret.secretid != null) {
+                    KnownSecrets[secret.secretid] = true;
+                    LogInfo($"learned secret {secret.secretid} during precognition");
+                }
             }
             else if (E.ID == "BeforeSecretForgotten" && Activated) {
                 // Register that the secret was forgotten
                 IBaseJournalEntry secret = E.GetParameter<IBaseJournalEntry>("Secret");
-                RevealedSecrets[secret.secretid] = false;
-                MetricsManager.LogInfo($"{Prefix}: forgot secret {secret.secretid} during precognition");
+                if (secret.secretid != null) {
+                    KnownSecrets[secret.secretid] = false;
+                    LogInfo($"forgot secret {secret.secretid} during precognition");
+                }
             }
 
             return base.FireEvent(E);
         }
 
-        private void RestoreSecretState(string secretid, bool known) {
-            MetricsManager.LogInfo($"{Prefix}: Restoring secret {secretid} (known = {known})");
-
-            // Find the note with the given ID and reveal or forget it
-            foreach (IBaseJournalEntry entry in JournalAPI.GetAllNotes()) {
-                if (entry.secretid == secretid) {
-                    if (known)
-                        entry.Reveal(silent: true);
-                    else
-                        entry.Forget(fast: true);
-                    break;
-                }
-            }
-        }
-
         private string GetSecretTransferKey(string secretid) {
             return $"{Prefix}_{secretid}";
+        }
+
+        private static void LogInfo(string Message) {
+            MetricsManager.LogInfo($"{Prefix}: {Message}");
+        }
+
+        private static void LogException(string Message, Exception ex) {
+            MetricsManager.LogException($"{Prefix}: {Message}", ex);
         }
     }
 }
--- PrecogSecrets/README.md
+++ PrecogSecrets/README.md
@@ -4,6 +4,3 @@ A mod to ensure that learning or forgetting a secret continues after using
 Precognition.
 
 Based on an idea by @slimegirls in the Caves of Qud Discord.
-
-**NOTE:** this mod is currently experimental and a little rough around the
-edges. Currently, it will only restore one secret learned during precognition.
