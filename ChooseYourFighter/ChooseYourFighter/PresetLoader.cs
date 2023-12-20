b6218c4ff52ca648a26bf8b2ede6c687d5014eaa
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2023-12-20 11:49:17 -0800

    Fix XML parsing errors.

--- ChooseYourFighter/ChooseYourFighter/PresetLoader.cs
+++ ChooseYourFighter/ChooseYourFighter/PresetLoader.cs
@@ -1,29 +1,89 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using XRL;
 using XRL.CharacterBuilds.Qud;
 
 namespace Kernelmethod.ChooseYourFighter {
+    public class NoOpXmlHandler : IDictionary<string, Action<XmlDataHelper>> {
+        private static readonly Dictionary<string, Action<XmlDataHelper>> EmptyDict = new Dictionary<string, Action<XmlDataHelper>>();
+
+        public void HandleNodes(XmlDataHelper xml) {
+            if (string.IsNullOrEmpty(xml.Name))
+                return;
+            xml.sanityChecks = false;
+            xml.HandleNodes(this);
+        }
+
+        public void Add(string key, Action<XmlDataHelper> action) { return; }
+        public void Clear() { return; }
+        public bool Remove(string key) => true;
+        IEnumerator<KeyValuePair<string, Action<XmlDataHelper>>> IEnumerable<KeyValuePair<string, Action<XmlDataHelper>>>.GetEnumerator() {
+            return EmptyDict.GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator() {
+            return EmptyDict.GetEnumerator();
+        }
+
+        public Action<XmlDataHelper> this[string key] {
+            get {
+                return HandleNodes;
+            }
+            set {
+                return;
+            }
+        }
+
+        public ICollection<string> Keys {
+            get {
+                return EmptyDict.Keys;
+            }
+        }
+        public ICollection<Action<XmlDataHelper>> Values {
+            get => EmptyDict.Values;
+        }
+        public int Count {
+            get => 0;
+        }
+        public bool IsReadOnly {
+            get => true;
+        }
+
+        public bool Contains(KeyValuePair<string, Action<XmlDataHelper>> kv) {
+            return true;
+        }
+        public bool ContainsKey(string key) => true;
+        public bool TryGetValue(string key, out Action<XmlDataHelper> value) {
+            value = HandleNodes;
+            return true;
+        }
+
+        public void CopyTo(KeyValuePair<string, Action<XmlDataHelper>>[] kv, int arrayIndex) { return; }
+        public void Add(KeyValuePair<string, Action<XmlDataHelper>> kv) { return; }
+        public bool Remove(KeyValuePair<string, Action<XmlDataHelper>> kv) => true;
+    }
+
     [HasModSensitiveStaticCache]
     public static class PresetLoader {
         public static Dictionary<string, QudPregenModule.QudPregenData> Presets = new Dictionary<string, QudPregenModule.QudPregenData>();
+        public static readonly NoOpXmlHandler NoOp = new NoOpXmlHandler();
+
+        public static void DefaultNodeHandler(XmlDataHelper xml) {
+            xml.sanityChecks = false;
+            xml.HandleNodes(XmlNodeHandlers);
+        }
 
         public static Dictionary<string, Action<XmlDataHelper>> XmlNodeHandlers => new Dictionary<string, Action<XmlDataHelper>>
         {
-            {
-                "embarkmodules",
-                delegate(XmlDataHelper xml)
-                {
-                    xml.HandleNodes(XmlNodeHandlers);
-                }
-            },
+            { "embarkmodules", DefaultNodeHandler },
             {
                 "module",
                 delegate(XmlDataHelper xml)
                 {
                     // Skip all modules except for QudPregenModule
-                    if (xml.GetAttribute("Class") != "XRL.CharacterBuilds.Qud.QudPregenModule")
-                        return;
+                    if (xml.GetAttribute("Class") != "XRL.CharacterBuilds.Qud.QudPregenModule") {
+                        xml.HandleNodes(NoOp);
+                    }
                     else {
                         var pregenModule = new QudPregenModule();
                         pregenModule.HandleNodes(xml);
